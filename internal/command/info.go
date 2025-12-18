4cfb3f01401f61ee546d5435b44363c67baa1a7b
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-12-18 13:41:32 +0700

    feat: embed info

--- internal/command/info.go
+++ internal/command/info.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/rs/zerolog/log"
@@ -34,13 +35,37 @@ func InfoHandler(s *discordgo.Session, i *discordgo.InteractionCreate, deps *com
 		gitHash = "unknown"
 	}
 
-	content := "This is a bot for downloading Instagram videos and more!\n\nCreated by yoketh.\nGit Hash: " + gitHash
+	content := "This is a bot for downloading Instagram videos and more!"
+
+	embed := &discordgo.MessageEmbed{
+		Title:       "Bot Information",
+		Description: content,
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:  "Repository",
+				Value: "[GitHub Repository](github.com/yokeTH/short-form-discord-app)",
+			},
+			{
+				Name:  "Usage Example",
+				Value: "`/ig <instagram_url>`",
+			},
+			{
+				Name:  "Git Hash",
+				Value: gitHash,
+			},
+		},
+		Color: 0x5865F2,
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "Created by yoketh",
+		},
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
 
 	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: content,
-			Flags:   discordgo.MessageFlagsEphemeral,
+			Embeds: []*discordgo.MessageEmbed{embed},
+			Flags:  discordgo.MessageFlagsEphemeral,
 		},
 	})
 
