fafe1f4d2130e22a69f9518fabdf6e650840a236
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-12-18 11:09:46 +0700

    feat: remove command

--- cmd/register/main.go
+++ cmd/register/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/rs/zerolog/log"
 
@@ -11,15 +12,22 @@ import (
 
 var (
 	BotToken = flag.String("token", "", "Bot access token")
+	Remove   = flag.Bool("remove", false, "Remove commands instead of registering them")
 )
 
 var s *discordgo.Session
 
-func init() { flag.Parse() }
-
 func init() {
+	flag.Parse()
+	token := *BotToken
+	if token == "" {
+		token = os.Getenv("DISCORD_BOT_TOKEN")
+	}
+	if token == "" {
+		log.Fatal().Msg("Bot token not provided via --token flag or DISCORD_BOT_TOKEN environment variable")
+	}
 	var err error
-	s, err = discordgo.New("Bot " + *BotToken)
+	s, err = discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Invalid bot parameters")
 	}
@@ -43,17 +51,29 @@ func main() {
 		log.Fatal().Err(err).Msg("Cannot open the session")
 	}
 
-	log.Info().Msg("Adding commands...")
-	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
-	for i, v := range commands {
-		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, "", v)
-		if err != nil {
-			log.Panic().Err(err).Str("command", v.Name).Msg("Cannot create command")
-		}
-		registeredCommands[i] = cmd
-	}
-
 	defer s.Close()
+
+	if *Remove {
+		log.Info().Msg("Removing commands...")
+		for _, v := range commands {
+			err := s.ApplicationCommandDelete(s.State.User.ID, "", v.ID)
+			if err != nil {
+				log.Panic().Err(err).Str("command", v.Name).Msg("Cannot remove command")
+			} else {
+				log.Info().Str("command", v.Name).Msg("Removed command")
+			}
+		}
+	} else {
+		log.Info().Msg("Adding commands...")
+		registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+		for i, v := range commands {
+			cmd, err := s.ApplicationCommandCreate(s.State.User.ID, "", v)
+			if err != nil {
+				log.Panic().Err(err).Str("command", v.Name).Msg("Cannot create command")
+			}
+			registeredCommands[i] = cmd
+		}
+	}
 
 	log.Info().Msg("Gracefully shutting down.")
 }
