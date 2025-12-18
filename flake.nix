fd2238faedbaf7c2a00420653cb10e43c99a1f72
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-12-18 11:21:59 +0700

    feat: docker + nix

--- flake.nix
+++ flake.nix
@@ -13,45 +13,24 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
 
-        # go-migrate-pg = pkgs.go-migrate.overrideAttrs (oldAttrs: {
-        #   tags = ["postgres"];
-        # });
-
-        # swag = pkgs.buildGoModule rec {
-        #   pname = "swag";
-        #   version = "v2.0.0-rc4";
-
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "swaggo";
-        #     repo = "swag";
-        #     rev = version;
-        #     sha256 = "sha256-3dX01PAVEkn8ciVNbIn1IwiSkwogPJLYNo6xTg9jhDA=";
-        #   };
-
-        #   vendorHash = "sha256-bUMW9wjPIT3JLMw9F/NvWqZv1M62o/Y4gIpp6XyHbek=";
-        #   subPackages = ["cmd/swag"];
-        # };
-
         app = pkgs.buildGoModule {
-          pname = "go-app";
+          pname = "short-form";
           version = "0.1.0";
 
           src = ./.;
 
-          vendorHash = "sha256-jx70HhXLbBt63Vt0iZK8aUwoQnpe57MIjUMzXsnaRgA=";
+          vendorHash = "sha256-MD8RJaVGnYj6nQ6Xgq8AthgFY+r/cuCmssOyfNrKC1s=";
 
           nativeBuildInputs = [
-            # swag
+            # pkgs.ffmpeg
           ];
 
           preBuild = ''
-            # Generate Swagger docs
-            # swag init -v3.1 -o docs -g main.go --parseDependency --parseInternal
           '';
 
           meta = with pkgs.lib; {
-            description = "Go application with Swagger documentation";
-            license = licenses.mit;
+            description = "short form video discord app";
+            license = licenses.bsd3;
           };
         };
       in {
@@ -68,18 +47,12 @@
 
             air
             # swag
-
-            # sqlc
-            # go-migrate-pg
-            # sql-formatter
-
             pre-commit
           ];
 
           shellHook = ''
             echo "Go development environment ready"
             echo "Go version: $(go version)"
-            # echo "Swag version: $(swag --version)"
           '';
         };
       }
